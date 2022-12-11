2cb20013d0a8d3fc5ae0a866a7916bd9155f93ae
Alex Simpson <alexkiwilson@gmail.com>
2022-12-10 23:08:55 -0500

    12.10.22

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingComponent } from './landing';
+
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: 'landing.component.html',
+  styleUrls: ['./../../styles.css']
+})
+export class LandingComponent {
+
+}
