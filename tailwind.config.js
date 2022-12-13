65cc1423c84847ae47d570d6466d6a5548f14933
Alex Simpson <alexkiwilson@gmail.com>
2022-12-13 07:10:02 -0500

    12.13.2022

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
         'inter': ['Inter', 'sans-serif'],
         'rock-salt': ['Rock Salt', 'cursive'],
       },
+      animation: {
+        'fadein': 'fadeIn 0.6s ease-in',
+      },
+
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        }
+
+      },
     },
   },
   plugins: [],
